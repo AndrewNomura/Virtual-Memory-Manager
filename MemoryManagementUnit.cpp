a76d6d3115c1f516a65d970f6ea8748749193e31
Andrew Nomura <drew97.nomura@gmail.com>
2019-05-09 11:57:51 -0700

    Create MemoryManagementUnit.cpp

---
+++ MemoryManagementUnit.cpp
@@ -0,0 +1,28 @@
+//
+//  MemoryManagementUnit.hpp
+//
+//  Created by Andrew Nomura on 5/9/19.
+//  Copyright © 2019 Andrew Nomura. All rights reserved.
+//
+
+#pragma
+
+template<typename T>
+class MemoryManagementUnit{
+public:
+    MemoryManagementUnit();
+    void clearTLB();
+    MemoryManagementUnit& instance();
+    MemoryManagementUnit& operator=(void);
+    int pageAccesses();
+    int pageFaults();
+    T read(void);
+    void tlbAccesses();
+    void tlbFaults();
+private:
+    int page_access_count_;
+    int page_in_faults_;
+    int tlb_:TLB;
+    int tlb_access_count_;
+    int tlb_faults_;
+};
