5c83aa23d81b587676984fdca6087af92e6bc0c2
Andrew Nomura <drew97.nomura@gmail.com>
2019-05-09 11:56:38 -0700

    Update and rename Hardware.hpp to Address.cpp

---
+++ Address.cpp
@@ -0,0 +1,20 @@
+//
+//  Address.cpp
+//
+//  Created by Andrew Nomura on 4/30/19.
+//  Copyright © 2019 Andrew Nomura. All rights reserved.
+//
+
+struct Address{
+public:
+    address():Word(){
+        uint32_t():int();
+        value_:uint32_t;
+    };
+    displacement():Word();
+    frame():Word();
+    page():Word();
+};
+
+
+
