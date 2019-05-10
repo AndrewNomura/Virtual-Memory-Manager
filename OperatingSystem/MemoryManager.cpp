8d93307b96e3aeaebb591c6c044e72ac05f4708a
Andrew Nomura <drew97.nomura@gmail.com>
2019-05-10 15:17:47 -0700

    Update MemoryManager.cpp

--- OperatingSystem/MemoryManager.cpp
+++ OperatingSystem/MemoryManager.cpp
@@ -4,4 +4,34 @@
 //  Created by Andrew Nomura on 5/9/19.
 //  Copyright © 2019 Andrew Nomura. All rights reserved.
 //
+#include <vector>
+using namespace std;
+
+
+class MemoryManager{
+public:
+    void makeValid();
+    void makeVector();
+    void page_in(); //select frame number; FCFS
+    
+    vector<int> frameVector;
+};
+
+void MemoryManager::makeVector(){            //vector of empty frames
+    for (int i = 0; i < 256; i++){
+        frameVector.push_back(0);
+    }
+}
+
+void MemoryManager::makeValid(){
+    //frame finder
+    for (int i = 0; i < 256; i++){
+        if (frameVector[i] == 0){
+            //RAM[frame #] = read(page)
+        }
+        else {
+            cout << 1 << endl;
+        }
+    }
+}
 
