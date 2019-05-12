7027107f62588b0db1ed57b22bc5e56bad2b2b69
Andrew Nomura <drew97.nomura@gmail.com>
2019-05-12 12:39:38 -0700

    Create Ram.hpp

---
+++ Ram.hpp
@@ -0,0 +1,30 @@
+//
+//  Ram.hpp
+//
+//  Created by Andrew Nomura on 5/9/19.
+//  Copyright © 2019 Andrew Nomura. All rights reserved.
+//
+
+#include <iostream>
+struct Status{
+public:
+    accessed:bool
+    dirty:bool
+};
+
+class Ram{
+public:
+    Ram& instance();
+    Ram& operator=(Ram& r){
+        Ram ram;
+        ram = this->ram + r.ram;
+    }
+    Ram()
+    read(void);
+status:array<Status, FRAME_SIZE>
+private:
+    RAM[256][256];  //RAM[frame #][offset]
+
+};
+
+
