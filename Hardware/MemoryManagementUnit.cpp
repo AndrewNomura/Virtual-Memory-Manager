9102f86fbc8a6b8f590d033e19342568d13bf31b
Andrew Nomura <drew97.nomura@gmail.com>
2019-05-12 12:32:02 -0700

    Update MemoryManagementUnit.cpp

--- Hardware/MemoryManagementUnit.cpp
+++ Hardware/MemoryManagementUnit.cpp
@@ -5,23 +5,33 @@
 //  Copyright © 2019 Andrew Nomura. All rights reserved.
 //
 
+
 class MemoryManagementUnit{
 public:
-    /*
+    
     MemoryManagementUnit(){}
     void clearTLB();
     MemoryManagementUnit& instance();
-    MemoryManagementUnit& operator=(void);
+   // MemoryManagementUnit& operator=(void);
     int pageAccesses();
     int pageFaults();
-    T read(void);
+    //T read(void);
     void tlbAccesses();
     void tlbFaults();
+    void read(Word& logicalAddress, PageTable& myPageTable, char data);
 private:
     int page_access_count_;
     int page_in_faults_;
-    int tlb_:TLB;
+    //int tlb_:TLB;
     int tlb_access_count_;
     int tlb_faults_;
-    */
+    
+    friend Pagetable;
+    //friend Address;
+    //friend Word;
 };
+
+void MemoryManagementUnit::read(Word& logicalAddress, PageTable& myPageTable, char data){
+    
+}
+
