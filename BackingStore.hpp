d1817c685c568137411a22039027151192ec9e85
Andrew Nomura <drew97.nomura@gmail.com>
2019-05-12 12:38:25 -0700

    Create BackingStore.hpp

---
+++ BackingStore.hpp
@@ -0,0 +1,24 @@
+//
+//  BackingStore.hpp
+//
+//  Created by Andrew Nomura on 5/9/19.
+//  Copyright © 2019 Andrew Nomura. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class BackingStore{
+public:
+    ~BackingStore(){}
+    BackingStore(){}
+    BackingStore& instance();
+    BackingStore& operator=();
+    void read();
+};
+
+void BackingStore::read(int logicalAddress, PageTable myPageTable, int data){
+    frame = PT(page).frame;
+    RAM[frame][offset];
+}
