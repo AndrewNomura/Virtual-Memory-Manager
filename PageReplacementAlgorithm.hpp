1b23bdf676ad6f8d412d6e09d364fcc281b82b4e
Andrew Nomura <drew97.nomura@gmail.com>
2019-05-12 12:41:42 -0700

    Create PageReplacementAlgorithm.hpp

---
+++ PageReplacementAlgorithm.hpp
@@ -0,0 +1,14 @@
+//
+//  PageReplacementAlgorithm.hpp
+//
+//  Created by Andrew Nomura on 5/9/19.
+//  Copyright © 2019 Andrew Nomura. All rights reserved.
+//
+
+
+class PageReplacementAlgorithm{
+public:
+    //FIFO
+private:
+    //LRU
+};
